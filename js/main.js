aa353f14466921d1fdb18f0fe1efe081407f545f
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-08-05 20:49:47 +0100

    changed querySelector for const text and used xWalk and yWalk to change textShadow

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const hero = document.querySelector(".hero");
-const text = document.querySelector(".h1");
+const text = document.querySelector("h1");
 const walk = 100;
 
 function shadow(e) {
@@ -12,6 +12,8 @@ function shadow(e) {
     }
     const xWalk = Math.round((x / width * walk) - (walk / 2));
     const yWalk = Math.round((y / width * walk) - (walk / 2));
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
     console.log(xWalk, yWalk)
 }
 
